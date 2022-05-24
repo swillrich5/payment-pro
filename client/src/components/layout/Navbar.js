0b61e748dfc18164144717b8330b668836492f74
Scott Willrich <swillrich@gmail.com>
2022-05-23 19:55:18 -0500

    tweaked navbar links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,13 +7,16 @@ import './Navbar.css';
 const Navbar = ({ title, icon }) => {
   return (
     <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
-    <Link to='/' ><h3 className="heading mr-2">  <img id="navbar-logo" src={thumbsUp} alt="pic" /></h3></Link>
+    <Link to='/' ><img id="navbar-logo" src={thumbsUp} Pro alt="pic" /></Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="nav navbar-nav">
             <ul className="navbar-nav">
+                <li className="nav-item">
+                <Link className="nav-link" to='/'>Payment Pro</Link>
+                </li>             
                 <li className="nav-item">
                     <Link className="nav-link" to='/'>Home</Link>
                 </li>
